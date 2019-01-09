bea5790551d1eda06b80dfc9de68c050313cd2dc
macos_gue <sonypiay@macos-gues-imac.local>
2019-01-09 23:41:48 +0700

    Initial Commit

---
+++ kooperasimercubuanaTests/kooperasimercubuanaTests.swift
@@ -0,0 +1,36 @@
+//
+//  kooperasimercubuanaTests.swift
+//  kooperasimercubuanaTests
+//
+//  Created by macos_gue on 1/9/19.
+//  Copyright © 2019 macos_gue. All rights reserved.
+//
+
+import XCTest
+@testable import kooperasimercubuana
+
+class kooperasimercubuanaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
